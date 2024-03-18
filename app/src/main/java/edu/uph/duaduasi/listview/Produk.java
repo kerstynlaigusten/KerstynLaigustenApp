e4ad3e392bdeee2b93dfd3c0313500eb50b0aee3
kerstynlaigusten <kerstynlaigusten@gmail.com>
2024-03-18 18:31:03 +0700

    Initial commit

---
+++ app/src/main/java/edu/uph/duaduasi/listview/Produk.java
@@ -0,0 +1,40 @@
+package edu.uph.duaduasi.listview;
+
+public class Produk {
+
+    public class Produk {
+        //attribut
+        private String namaProduk;
+        private int hargaProduk;
+        private int gambarProduk;
+
+        public Produk(String namaProduk, int hargaProduk, int gambarProduk) {
+            this.namaProduk = namaProduk;
+            this.hargaProduk = hargaProduk;
+            this.gambarProduk = gambarProduk;
+        }
+
+        public String getNamaProduk() {
+            return namaProduk;
+        }
+
+        public void setNamaProduk(String namaProduk) {
+            this.namaProduk = namaProduk;
+        }
+
+        public int getHargaProduk() {
+            return hargaProduk;
+        }
+
+        public void setHargaProduk(int hargaProduk) {
+            this.hargaProduk = hargaProduk;
+        }
+
+        public int getGambarProduk() {
+            return gambarProduk;
+        }
+
+        public void setGambarProduk(int gambarProduk) {
+            this.gambarProduk = gambarProduk;
+        }
+    }
